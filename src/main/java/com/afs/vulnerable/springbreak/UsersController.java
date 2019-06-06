2aea1f011312f0f82ea4c0e0679e8694fc5e49d5
Juan Larosa <juan.larosa@despegar.com>
2019-06-06 12:07:09 -0300

    Add /

--- src/main/java/com/afs/vulnerable/springbreak/UsersController.java
+++ src/main/java/com/afs/vulnerable/springbreak/UsersController.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,9 +13,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class UsersController {
     @Autowired
     UsersService service;
+
     @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public ResponseEntity<User> getUserInfo(@Param("id") String id) {
+    @CrossOrigin(origins = "*")
+    public ResponseEntity getUserInfo(@Param("id") String id, @Param("token") String token) {
+        if(!token.equals("123")) return new ResponseEntity("", HttpStatus.BAD_REQUEST);
         return new ResponseEntity<User>(service.getUser(id), HttpStatus.OK);
+    }
+
+    // /users?id=1&token=123
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @CrossOrigin(origins = "*")
+    public ResponseEntity getInfo() {
+        return new ResponseEntity("prueba", HttpStatus.OK);
     }
 
 }
